032599524efcafe49bb9ecaf45e95687bcb1e6a8
kangzBredz04 <wabredz1234@gmail.com>
2024-08-22 22:31:49 +0700

    cleanup: remove unnecessary code from index.js

--- api/index.js
+++ api/index.js
@@ -2,21 +2,16 @@ import express from "express";
 import "dotenv/config";
 
 import BookRoute from "./routes/book-route.js";
+import MemberRoute from "./routes/member-route.js";
 
 const app = express();
-
 app.use(express.json());
 
 const router = express.Router();
 app.use("/api", router);
 
 router.use("/book", BookRoute);
-
-router.use("/getting-started", async (_req, res) => {
-  res.status(200).json({
-    message: "Selamat Datang Di Website Sport Collection",
-  });
-});
+router.use("/member", MemberRoute);
 
 app.listen(process.env.API_PORT, () =>
   console.log(`Server berhasil dijalankan pada port ${process.env.API_PORT}`)
