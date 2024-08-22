fd61254dbf5660ded639f441639e7f080727d15f
kangzBredz04 <wabredz1234@gmail.com>
2024-08-22 21:31:39 +0700

    chore: update database configuration in db.js

--- api/config/db.js
+++ api/config/db.js
@@ -1,14 +1,15 @@
-import { Sequelize } from "sequelize";
 import "dotenv/config";
+import pkg from "pg";
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-  }
-);
+const { Pool } = pkg;
 
-module.exports = sequelize;
+export const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+// Koneksi ke database
+pool.connect().then(() => console.log("Berhasil terhubung ke basis data."));
